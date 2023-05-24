ae6b457819f59a5f2df719e03a3ce40d60ee59a5
Liparulo <laura.liparulo@capgemini.com>
2023-05-25 01:55:07 +0200

    tests

--- src/main/java/de/demo/resource/MessageResource.java
+++ src/main/java/de/demo/resource/MessageResource.java
@@ -9,6 +9,7 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 /**
  * A simple resource showing the last price.
@@ -28,17 +29,16 @@ public class MessageResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String last() {
         return consumer.getLastMessage();
-//        return Response.ok(message).build();
     }
      
     
     @POST
     @Path("send")
     @Consumes(MediaType.TEXT_PLAIN)
-//    @Produces(MediaType.APPLICATION_JSON)
-    public void sendMessage(String message) {
+    @Produces("text/plain")
+    public Response sendMessage(String message) {
          producer.sendMessageBody(message);
-//         return Response.status(201).build();
+         return Response.ok(message+" sent!").build();
     }
     
   

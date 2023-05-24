c752e134eb12efdf9502b9c502ba77d838c4e16b
Liparulo <laura.liparulo@capgemini.com>
2023-05-25 00:27:06 +0200

    tests

---
+++ src/main/java/de/demo/jms/MessageProducer.java
@@ -0,0 +1,27 @@
+package de.demo.jms;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.jms.ConnectionFactory;
+import jakarta.jms.JMSContext;
+import jakarta.jms.JMSProducer;
+import jakarta.jms.Queue;
+
+@ApplicationScoped
+public class MessageProducer {
+	public final static String PRODUCER_QUEUE = "messages-send-queue";
+	
+
+    @Inject
+    ConnectionFactory connectionFactory;
+
+
+    public void sendMessageBody(String body) {
+        try (JMSContext context = connectionFactory.createContext(JMSContext.AUTO_ACKNOWLEDGE)) {
+            Queue destination = context.createQueue(PRODUCER_QUEUE);
+            JMSProducer producer = context.createProducer();
+
+            producer.send(destination, body);
+        }
+    }
+}

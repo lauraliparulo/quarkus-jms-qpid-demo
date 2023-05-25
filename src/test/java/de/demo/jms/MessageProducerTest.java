3463c32ece1171426179c2fee7304f6e2ba0e045
Liparulo <laura.liparulo@capgemini.com>
2023-05-25 02:02:58 +0200

    tests

--- src/test/java/de/demo/jms/MessageProducerTest.java
+++ src/test/java/de/demo/jms/MessageProducerTest.java
@@ -1,6 +1,5 @@
 package de.demo.jms;
 
-import static de.demo.jms.QpidJmsTestSupport.RECEIVE_MESSAGE_ENDPOINT_PATH;
 import static de.demo.jms.QpidJmsTestSupport.SEND_MESSAGE_ENDPOINT_PATH;
 
 import org.junit.jupiter.api.Assertions;
@@ -10,7 +9,7 @@ import io.quarkus.artemis.test.ArtemisTestResource;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
+import jakarta.inject.Inject;
 import jakarta.jms.JMSConsumer;
 import jakarta.jms.JMSContext;
 import jakarta.jms.Queue;
@@ -20,19 +19,23 @@ import jakarta.ws.rs.core.Response.Status;
 @QuarkusTestResource(ArtemisTestResource.class)
 public class MessageProducerTest {
 	
+	@Inject
+	MessageConsumer consumer;
+	
     @Test
     public void testSend() throws Exception {
         String body = QpidJmsTestSupport.generateBody();
 
         io.restassured.response.Response response = RestAssured.with().body(body).post(SEND_MESSAGE_ENDPOINT_PATH);
         Assertions.assertEquals(Status.OK.getStatusCode(), response.statusCode());
-
-        try (JMSContext context = QpidJmsTestSupport.createContext()) {
-            Queue destination = context.createQueue(MessageProducer.PRODUCER_QUEUE);
-            JMSConsumer consumer = context.createConsumer(destination);
-            Assertions.assertEquals(body, consumer.receive().toString(), "Received body did not match that sent");
-            
-        }
+        Assertions.assertEquals(body, consumer.getLastMessage(), "Received body did not match that sent");
+        
+//        try (JMSContext context = QpidJmsTestSupport.createContext()) {
+//            Queue destination = context.createQueue(MessageProducer.PRODUCER_QUEUE);
+//            JMSConsumer consumer = context.createConsumer(destination);
+//            
+//            
+//        }
     }
     
 
